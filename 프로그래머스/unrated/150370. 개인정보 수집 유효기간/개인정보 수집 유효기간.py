15c7fe4bb74a4b5e08d9c919f024f2e861df53fa
sudog1 <85793331+sudog1@users.noreply.github.com>
2023-08-30 13:35:16 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.17 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -0,0 +1,15 @@
+def convert(date, weight):
+    return sum(map(lambda x, y: int(x)*y, date.split('.'), weight))
+
+def solution(today, terms, privacies):
+    weight = [12*28, 28, 1]
+    today = convert(today, weight)
+    terms = list(map(lambda x: x.split(), terms))
+    terms_dict = {f'{term}': int(expi)*28 for term, expi in terms}
+    answer = []
+    for i, privacy in enumerate(privacies):
+        date, term = privacy.split()
+        if today - convert(date, weight) >= terms_dict[term]:
+            answer.append(i+1)
+            
+    return answer
