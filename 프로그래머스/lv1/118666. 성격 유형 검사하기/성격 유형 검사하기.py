c7dbe140b0628d20bb76136069ab1a26b80214f7
sudog1 <85793331+sudog1@users.noreply.github.com>
2023-08-30 12:44:00 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.21 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -0,0 +1,21 @@
+def solution(survey, choices):
+    pers_type = ['R', 'T', 'C', 'F', 'J', 'M', 'A', 'N']
+    type_score = {
+        'R': 0,
+        'C': 0,
+        'J': 0,
+        'A': 0,
+    }
+    for i, s in enumerate(survey):
+        if s[0] in type_score:
+            type_score[s[0]] += 4-choices[i]
+        else:
+            type_score[s[1]] -= 4-choices[i]
+            
+    result = []
+    for i in range(0, len(pers_type), 2):
+        if type_score[pers_type[i]] >= 0:
+            result.append(pers_type[i])
+        else:
+            result.append(pers_type[i+1])
+    return ''.join(result)
