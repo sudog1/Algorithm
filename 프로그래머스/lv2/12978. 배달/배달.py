eef8ebf930476424c667a3d1ac586ee97fb07381
sudog <85793331+sudog1@users.noreply.github.com>
2023-09-19 11:28:54 +0900

    [level 2] Title: 배달, Time: 0.09 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12978. 배달/배달.py
+++ 프로그래머스/lv2/12978. 배달/배달.py
@@ -1,35 +1,33 @@
 import heapq
-from math import inf
 
-def dijk(graph, start, N):
-    dist = [inf]*(N+1)
-    dist[start] = 0
-    prev = []
-    heapq.heappush(prev, (dist[start], start))
-    while prev:
-        cur_dist, cur_node = heapq.heappop(prev)
-        if cur_dist > dist[cur_node]:
+
+def dijkstra(N, graph, cost, K):
+    dist = [K+1]*(N+1)
+    dist[1] = 0
+    heap = [[dist[1], 1]]
+    while heap:
+        d, node = heapq.heappop(heap)
+        if dist[node] < d:
             continue
-        for i, d in enumerate(graph[cur_node]):
-            if i and d < 10001:
-                if dist[cur_node] + d < dist[i]:
-                    dist[i] = dist[cur_node] + d
-                    heapq.heappush(prev, (dist[i], i))
-                    
+        for adj in graph[node]:
+            if dist[adj] > d+cost[node][adj]:
+                dist[adj] = d+cost[node][adj]
+                heapq.heappush(heap, [dist[adj], adj])
     return dist
-        
+
+
 def solution(N, road, K):
-    answer = 0
-    graph = [[10001]*(N+1) for _ in range(N+1)]
-    
-    for s,e,d in road:
-        graph[s][e] = min(graph[s][e], d)
-        graph[e][s] = min(graph[e][s], d)
-            
-    dist = dijk(graph, 1, N)
-    
-    for i, d in enumerate(dist):
-        if d <= K:
-            answer += 1
-    
-    return answer
+    graph = [[] for _ in range(N+1)]
+    cost = [[0]*(N+1) for _ in range(N+1)]
+    for a, b, c in road:
+        if cost[a][b]:
+            if cost[a][b] > c:
+                cost[a][b] = c
+                cost[b][a] = c
+        else:
+            graph[a].append(b)
+            graph[b].append(a)
+            cost[a][b] = c
+            cost[b][a] = c
+    dist = dijkstra(N, graph, cost, K)
+    return len(list(filter(lambda x: x <= K, dist)))
