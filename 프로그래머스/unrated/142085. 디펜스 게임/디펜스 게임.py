7909bde1f3773442114e152ad062a63804da8ff2
sudog <85793331+sudog1@users.noreply.github.com>
2023-09-25 09:43:40 +0900

    [unrated] Title: 디펜스 게임, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.py
@@ -0,0 +1,16 @@
+import heapq
+
+
+def solution(n, k, enemy):
+    answer = 0
+    invinc = enemy[:k]
+    heapq.heapify(invinc)
+    for i in range(k, len(enemy)):
+        if enemy[i] > invinc[0]:
+            n -= heapq.heappop(invinc)
+            heapq.heappush(invinc, enemy[i])
+        else:
+            n -= enemy[i]
+        if n < 0:
+            return i
+    return len(enemy)
