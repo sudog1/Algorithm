7f398c5d171e883d64bc4619d88de0e5e23be2c2
sudog <85793331+sudog1@users.noreply.github.com>
2023-09-25 10:30:09 +0900

    [unrated] Title: 두 원 사이의 정수 쌍, Time: 404.93 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
@@ -0,0 +1,15 @@
+# from math import floor, sqrt
+
+
+def solution(r1, r2):
+    answer = 0
+    for x in range(1, r2+1):
+        dist = int((r2*r2 - x*x)**.5)
+        if x < r1:
+            temp = (r1*r1 - x*x)**.5
+            dist -= int(temp)
+            dist += 1 if temp.is_integer() else 0
+        else:
+            dist += 1
+        answer += dist
+    return answer*4
